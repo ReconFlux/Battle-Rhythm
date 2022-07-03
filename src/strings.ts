81439824716c2d7e20df9c46b955d7cafb7d5bc2
Stephen Burtrum <tsgtrecon117@gmail.com>
2022-07-03 17:57:20 -0500

    updated strings

--- src/strings.ts
+++ src/strings.ts
@@ -19,11 +19,11 @@ const Strings = {
     LineOfEfforts: ContextInfo.webServerRelativeUrl + "/_layouts/15/FldEdit.aspx?List=%7B24613378-6DC2-4604-B24F-992E0DC64A50%7D&Field=LinesOfEffort", // Static URL
     Priorities: ContextInfo.webServerRelativeUrl + "/_layouts/15/FldEdit.aspx?List=%7B24613378-6DC2-4604-B24F-992E0DC64A50%7D&Field=Priorities", // Static URL
     ProjectName: "Battle Rhythm",
-    ProjectDescription: "A custom sharepoint solution that provides collaboration of leadership and objectives, giving a Battle Rhythm.",
+    ProjectDescription: "A custom sharepoint application that provides information collaboration for leadership, objectives, and goals into a Dahboard view and timeline.",
     Lists: {
         BREvents: "Events",
         Templates: "Templates",
-        Settings: "Settings"
+        Settings: "Settings" // Settings for the Application
     },
     SolutionUrl: ContextInfo.webServerRelativeUrl + "/SiteAssets/sp-BattleRhythm/index.html",
     SupportEmail: "stephenburtrum@burtrumtech.onmicrosoft.com",
