0ba873fc4d02c67668df3931dd18f5e181aa8fe7
Stephen Burtrum <tsgtrecon117@gmail.com>
2022-07-03 17:55:30 -0500

    Project Initialization

---
+++ src/strings.ts
@@ -0,0 +1,35 @@
+import { ContextInfo } from "gd-sprest-bs";
+
+// Updates the strings for SPFx
+export const setContext = (context) => {
+    // Set the page context
+    ContextInfo.setPageContext(context);
+
+    // Update the values
+    Strings.SolutionUrl = ContextInfo.webServerRelativeUrl + "/SiteAssets/sp-BattleRhythm/index.html";
+}
+
+// Strings
+const Strings = {
+    AppElementId: "sp-BattleRhythm",
+    DateFormat: "MM/DD/YYYY",
+    GlobalVariable: "SPDashboard",
+
+    // TODO - Making the List Settings URL Dynamic
+    LineOfEfforts: ContextInfo.webServerRelativeUrl + "/_layouts/15/FldEdit.aspx?List=%7B24613378-6DC2-4604-B24F-992E0DC64A50%7D&Field=LinesOfEffort", // Static URL
+    Priorities: ContextInfo.webServerRelativeUrl + "/_layouts/15/FldEdit.aspx?List=%7B24613378-6DC2-4604-B24F-992E0DC64A50%7D&Field=Priorities", // Static URL
+    ProjectName: "Battle Rhythm",
+    ProjectDescription: "A custom sharepoint solution that provides collaboration of leadership and objectives, giving a Battle Rhythm.",
+    Lists: {
+        BREvents: "Events",
+        Templates: "Templates",
+        Settings: "Settings"
+    },
+    SolutionUrl: ContextInfo.webServerRelativeUrl + "/SiteAssets/sp-BattleRhythm/index.html",
+    SupportEmail: "stephenburtrum@burtrumtech.onmicrosoft.com",
+    DateTimeFormat: "YYYY-MM-DD  HH:mm",
+    Version: "3.7.5",
+    WebConfigUrl: ContextInfo.webServerRelativeUrl + "/SiteAssets/sp-BattleRhythm/config.json",
+
+}
+export default Strings;
