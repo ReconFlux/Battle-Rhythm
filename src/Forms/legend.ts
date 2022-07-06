d7590cb2d44703971dc51a8f596bd092ca5cf4d0
Stephen Burtrum <tsgtrecon117@gmail.com>
2022-07-06 07:37:19 -0500

    refactoring the legend.

--- src/Forms/legend.ts
+++ src/Forms/legend.ts
@@ -9,10 +9,8 @@ export class Legend {
     private _elBase = document.createElement('div');
 
     constructor(el: HTMLElement) {
-
-
         this.render(el);
-
+        el.id = "mydiv";
     }
 
     private render(el: HTMLElement) {
@@ -85,4 +83,48 @@ export class Legend {
 
     }
 
+
+}
+//Make the DIV element draggagle:
+dragElement(document.getElementById("mydiv"));
+let toasthead = document.querySelector(".toast-header") as HTMLElement;
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (toasthead) {
+        /* if present, the header is where you move the DIV from:*/
+        toasthead.onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
